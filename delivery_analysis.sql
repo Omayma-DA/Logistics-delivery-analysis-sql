b151324f768756f1dc1b0088289c07b9e7b24b27
Omayma-DA <omaymachad4@gmail.com>
2026-01-01 22:31:28 -0500

    Update delivery_analysis.sql
    Add header and first query

--- delivery_analysis.sql
+++ delivery_analysis.sql
@@ -1,1 +1,9 @@
+-- Logitics Delivery Performance Analysis
+-- Author: Omayma Chadli
+-- Dataset: deliveries.csv
+-- Purpose: Analyze late deliveries and performance patterns
+
+-- View all records
+SELECT * FROM deliveries;
+
 
